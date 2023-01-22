d84d0840e9bc98291e44e7166ba443002d5bd9cf
VadymMintenko <vad.m1ntenko@gmail.com>
2023-01-22 15:11:17 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-// іваіав
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+KEY = '33034390-7e7038dc39440b662093bd231';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const formEl = document.querySelector('.search-form');
+const divEl = document.querySelector('.gallery');
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const inputValue = evt.target.value;
+  wrap(inputValue).then(resp => createCard(resp));
+}
+
+async function wrap(param) {
+  const response = await axios.get(`${BASE_URL}?key=${KEY}&q=${param}`);
+  return response.hits;
+}
+
+function createCard(arr) {
+  const murkup = arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return ` <div class="photo-card">
+      <img src="${webformatURL}" alt="" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>${views}</b>
+        </p>
+        <p class="info-item">
+          <b>${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>${downloads}</b>
+        </p>
+      </div>
+    </div>`;
+      }
+    )
+    .join('');
+  divEl.innerHTML = murkup;
+}
