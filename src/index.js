dcdc182e46153857edc9dd72f1b0b79d2170021d
VadymMintenko <vad.m1ntenko@gmail.com>
2023-01-26 19:55:25 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
 KEY = '33034390-7e7038dc39440b662093bd231';
 const BASE_URL = 'https://pixabay.com/api/';
@@ -16,25 +18,42 @@ button.addEventListener('click', loadMore);
 let inputValue = '';
 let page = 1;
 
+// let lightBox = new SimpleLightbox('.gallery a', {
+//   captionDelay: 250,
+// });
+
 function onInput(evt) {
   inputValue = evt.target.value;
+  if (inputValue === '') {
+    clearMarkup();
+    button.hidden = true;
+    page = 1;
+  }
 }
 
 function onSubmit(evt) {
   evt.preventDefault();
-  wrap(page, inputValue).then(resp => createCard(resp));
+  getImages(page, inputValue).then(resp => {
+    if (resp.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    createCard(resp);
+  });
 }
 
-async function wrap(page = 1, param) {
+async function getImages(page = 1, param) {
   const response = await axios.get(
-    `${BASE_URL}?key=${KEY}&q=${param}&page=${page}&per_page=100`
+    `${BASE_URL}?key=${KEY}&q=${param}&page=${page}&per_page=40`
   );
   console.log(response);
-  return response.data.hits;
+  return response.data;
 }
 
 function createCard(arr) {
-  const murkup = arr
+  const murkup = arr.hits
     .map(
       ({
         webformatURL,
@@ -45,23 +64,25 @@ function createCard(arr) {
         comments,
         downloads,
       }) => {
-        return ` <div class="photo-card">
-      <img src="${webformatURL}" alt="" loading="lazy" />
-      <div class="info">
-        <p class="info-item">
-          <b>likes:${likes}</b>
+        return `<a href='${largeImageURL}'>
+      <div class='photo-card'>
+      <img src=${webformatURL} alt='${tags}' loading='lazy' />
+      <div class='info'>
+        <p class='info-item'>
+          <b>Likes</b>${likes}
         </p>
-        <p class="info-item">
-          <b>views:${views}</b>
+        <p class='info-item'>
+          <b>Views</b>${views}
         </p>
-        <p class="info-item">
-          <b>comments:${comments}</b>
+        <p class='info-item'>
+          <b>Comments</b>${comments}
         </p>
-        <p class="info-item">
-          <b>${downloads}</b>
+        <p class='info-item'>
+          <b>Downloads</b>${downloads}
         </p>
       </div>
-    </div>`;
+    </div>
+  </a>`;
       }
     )
     .join('');
@@ -71,11 +92,19 @@ function createCard(arr) {
 
 function loadMore() {
   page += 1;
-  wrap(page, inputValue).then(data => {
-    createCard(data);
-    console.log(data);
-    if (data.hits.length === 0) {
+  getImages(page, inputValue)
+    .then(data => {
+      if (data.hits.length < 40 || data.totalHits === 500) {
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+      createCard(data);
       button.hidden = true;
-    }
-  });
+    })
+    .catch(() => {});
+}
+
+function clearMarkup() {
+  divEl.innerHTML = '';
 }
